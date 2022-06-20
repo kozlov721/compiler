4ab166889d9833fa4f237dbcdce0d2f90fa3b9c9
kozlov721 <kozlovsky.m7@gmail.com>
2022-06-20 16:50:50 +0200

    tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,29 @@
+import Compiler
+import System.Process
+import System.Directory
+import GHC.IO.Exception (ExitCode(..))
+
+toExe :: String -> String -> IO ()
+toExe file out = do
+    src <- readFile file
+    let asmFile = out ++ ".s"
+    let asm = compile src
+    writeFile asmFile asm
+    callProcess "gcc" [asmFile, "-o", out]
+
+test :: String -> IO ()
+test file = do
+    let out = init . init $ file
+    toExe file out
+    (code, _, _) <- readProcessWithExitCode out [] ""
+    putStr file
+    case code of
+        ExitFailure 42 -> putStrLn ": OK"
+        _ -> putStrLn ": ERR"
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    files <- listDirectory "test/resources"
+    mapM_ (test . ("test/resources/"++))
+        $ filter (\x -> [last . init $ x, last x] == ".c") files
